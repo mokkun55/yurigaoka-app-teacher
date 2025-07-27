dc955060b7f4bd7d530669a7e5ad632bfe1d49c2
もっくん <mokkunpc@gmail.com>
2025-07-27 20:51:03 +0900

    feat: linterの設定、layoutの作成

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from 'eslint-plugin-storybook'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
@@ -22,7 +20,6 @@ const eslintConfig = [
   },
   js.configs.recommended,
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  ...storybook.configs['flat/recommended'],
   {
     plugins: {
       import: importPlugin,
