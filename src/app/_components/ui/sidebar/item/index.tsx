e94035e3cba086c7e8836a5197b71540a12dff01
もっくん <mokkunpc@gmail.com>
2025-07-27 22:15:06 +0900

    wip sidebarの作成

---
+++ src/app/_components/ui/sidebar/item/index.tsx
@@ -0,0 +1,74 @@
+import styles from './styles.module.css'
+import Link from 'next/link'
+import { House, LampCeiling, FileText, Users, Settings, Newspaper, Key, UserRoundCog } from 'lucide-react'
+
+type Item = {
+  href: string
+  label: string
+  icon: React.ReactNode
+}
+
+const items: Item[] = [
+  {
+    href: '/',
+    label: 'ホーム',
+    icon: <House />,
+  },
+  {
+    href: '/students/on-leave',
+    label: '帰省者一覧',
+    icon: <LampCeiling />,
+  },
+  {
+    href: '/reports',
+    label: 'すべての申請',
+    icon: <FileText />,
+  },
+  {
+    href: '/students',
+    label: '寮生一覧',
+    icon: <Users />,
+  },
+  {
+    href: '/notifications',
+    label: 'お知らせ管理',
+    icon: <Newspaper />,
+  },
+  {
+    href: '/code',
+    label: '招待コードの管理',
+    icon: <Key />,
+  },
+  {
+    href: '/students/leaders',
+    label: '指導寮生の管理',
+    icon: <UserRoundCog />,
+  },
+  {
+    href: '/settings',
+    label: '設定',
+    icon: <Settings />,
+  },
+]
+
+type Props = {
+  label: Item['label']
+  isActive: boolean
+}
+
+export const SidebarItem = ({ label, isActive }: Props) => {
+  const item = items.find((item) => item.label === label)
+
+  if (!item) {
+    return null
+  }
+
+  const isActiveClass = isActive ? styles.sidebarItemActive : ''
+
+  return (
+    <Link href={item.href} className={`${styles.sidebarItem} ${isActiveClass}`}>
+      <div className={styles.sidebarItemIcon}>{item.icon}</div>
+      <h2 className={styles.sidebarItemLabel}>{item.label}</h2>
+    </Link>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { Toaster } from 'react-hot-toast'
 import dayjs from 'dayjs'
-
+import '@mantine/core/styles.css'
+import { MantineProvider } from '@mantine/core'
+import styles from './layout.module.css'
+import { SidebarItem } from './_components/ui/sidebar/item'
 dayjs.locale('ja')
 
 export const metadata: Metadata = {
@@ -17,10 +20,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className="">
-        <div className="">
-          <Toaster />
-          {children}
+      <body>
+        <div className={styles.container}>
+          <MantineProvider>
+            <Toaster />
+            {/* サイドバー */}
+            <div className={styles.sidebar}>
+              <div className={styles.sidebarHeader}>
+                <h1 className={styles.sidebarHeaderTitle}>帰省届管理ツール</h1>
+              </div>
+
+              <div className={styles.sidebarContent}>
+                <div className={styles.sidebarItem}>
+                  <SidebarItem label="ホーム" isActive={true} />
+                  <SidebarItem label="帰省者一覧" isActive={false} />
+                  <SidebarItem label="すべての申請" isActive={false} />
+                  <SidebarItem label="寮生一覧" isActive={false} />
+                  <SidebarItem label="お知らせ管理" isActive={false} />
+                  <SidebarItem label="招待コードの管理" isActive={false} />
+                  <SidebarItem label="指導寮生の管理" isActive={false} />
+                  <SidebarItem label="設定" isActive={false} />
+                </div>
+              </div>
+            </div>
+
+            {/* メインコンテンツ */}
+            <div className={styles.mainContent}>
+              {/* ヘッダー */}
+              <div className={styles.header}>
+                <h2>現在アクティブなタブ</h2>
+              </div>
+              <div className={styles.content}>{children}</div>
+            </div>
+          </MantineProvider>
         </div>
       </body>
     </html>
