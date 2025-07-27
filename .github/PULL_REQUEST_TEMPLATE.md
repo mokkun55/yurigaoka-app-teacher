e8ff1d5ce77e58ed6d822de2f64d80e86852db14
もっくん <mokkunpc@gmail.com>
2025-07-27 20:18:19 +0900

    first commit

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,26 @@
+<!-- I want to review in Japanese. -->
+
+# 内容
+
+# レビューポイント
+
+## 以下、Copilotに知っておいてほしいこと
+
+<!-- I want to review in Japanese. -->
+<!-- for GitHub Copilot review rule -->
+<!--
+レビューする際には、以下のprefix(接頭辞)をつけてください
+[must]
+[imo] (in my opinion)
+[nits](nitpick)
+[ask]
+[fyi]
+-->
+<!--
+レビュー者としてのあなた：
+- ポジティブで建設的なコミュニケーションを心がけます
+- 技術的な問題点は明確に指摘しつつも、相手を尊重する表現を使います
+- チームの雰囲気作りを大切にし、メンバーのモチベーション向上に貢献します
+ -->
+<!-- for GitHub Copilot review  rule-->
+<!-- I want to review in Japanese. -->
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,58 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import globals from 'globals'
+import js from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  {
+    ignores: ['.next/', 'node_modules', 'dist', 'build', 'public/', 'src/utils/supabase/database.types.ts'],
+  },
+  js.configs.recommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...storybook.configs['flat/recommended'],
+  {
+    plugins: {
+      import: importPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }],
+      // "import/no-unused-modules": ["error", { unusedExports: true }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  prettierConfig,
+]
 
-export default eslintConfig;
+export default eslintConfig
