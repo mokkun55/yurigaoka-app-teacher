582b75f7208bbedc08b24bc0a6a7b70042665fce
もっくん <mokkunpc@gmail.com>
2025-07-28 13:16:10 +0900

    feat: ホーム画面のレイアウト

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,24 @@
+import styles from './styles.module.css'
+
 export default function Home() {
-  return <div>ホーム</div>
+  return (
+    <div className={styles.container}>
+      {/* 寮の状態 */}
+      <div className={styles.dormitoryStatus}>
+        <div className={styles.title}>寮の状態 (本日)</div>
+        <div className={styles.statusContainer}>
+          {/* 在寮人数 */}
+          {/* 帰省者数 */}
+        </div>
+      </div>
+
+      {/* 承認待ちの申請 */}
+      <div className={styles.approvalItems}>
+        <div className={styles.title}>
+          承認待ちの申請 <span className={styles.count}>(7件)</span>
+        </div>
+        <div className={styles.approvalList}>{/* アイテム */}</div>
+      </div>
+    </div>
+  )
 }
