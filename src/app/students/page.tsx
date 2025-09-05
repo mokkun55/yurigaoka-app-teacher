44bfbca1f92ea4712597054fefc8a1e9d9e6787e
もっくん <mokkunpc@gmail.com>
2025-09-05 15:01:53 +0900

    feat: バッジの作成

--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -1,3 +1,20 @@
+import Badge from '../_components/ui/badge'
+
 export default function StudentsPage() {
-  return <div>寮生一覧</div>
+  return (
+    <div>
+      <Badge variant="pending" />
+      <Badge variant="approved" />
+      <Badge variant="rejected" />
+      <Badge variant="canceled" />
+      <Badge variant="stay" />
+      <Badge variant="leave" />
+      <Badge variant="pending" size="big" />
+      <Badge variant="approved" size="big" />
+      <Badge variant="rejected" size="big" />
+      <Badge variant="canceled" size="big" />
+      <Badge variant="stay" size="big" />
+      <Badge variant="leave" size="big" />
+    </div>
+  )
 }
